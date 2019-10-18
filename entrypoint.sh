cbceb53f7939d60b0933ae3ac125c1a94ddf0149
Aaron <AaronBatilo@gmail.com>
2019-10-18 07:04:43 -0600

    feat: return latest_tag_published_at

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ repo="$INPUT_REPO"
 
 release_json=$(curl https://api.github.com/repos/$owner/$repo/releases)
 latest_tag=$(echo "$release_json" | jq -r '.[0].tag_name')
+latest_tag_published_at=$(echo "$release_json" | jq -r '.[0].published_at')
 
 echo ::set-output name=latest_tag::"$latest_tag"
+echo ::set-output name=latest_tag_published_at::"$latest_tag_published_at"
