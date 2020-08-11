baa4c61415762081c02f4d819b548dae9ce39e29
Michael <michaelzanggl@gmail.com>
2020-08-11 21:13:18 +0900

    echo out target_commitish

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,9 @@ repo="$INPUT_REPO"
 
 release_json=$(curl https://api.github.com/repos/$owner/$repo/releases)
 latest_tag=$(echo "$release_json" | jq -r '.[0].tag_name')
-target_commitish=$(echo "$release_json" | jq -r '.[0].target_commitish')
 latest_tag_published_at=$(echo "$release_json" | jq -r '.[0].published_at')
+target_commitish=$(echo "$release_json" | jq -r '.[0].target_commitish')
 
 echo ::set-output name=latest_tag::"$latest_tag"
 echo ::set-output name=latest_tag_published_at::"$latest_tag_published_at"
+echo ::set-output name=target_commitish::"$target_commitish"
